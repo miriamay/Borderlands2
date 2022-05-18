3a4cfe763852c9a03e1d6f53bf23ccabc1eb81a6
miriamay <105532752+miriamay@users.noreply.github.com>
2022-05-18 15:21:02 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let is_running = false;
 let demo_button = document.getElementById("start_demo");
 let currentMovement = "1";
 
-console.log("v5");
+console.log("v6");
 
 const gainNode = new Tone.Gain(0).toDestination();
 const gainNode2 = new Tone.Gain(0).connect(gainNode);
