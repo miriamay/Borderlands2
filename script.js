4b183232961669024808ec52c005626d7a54e1ec
miriamay <105532752+miriamay@users.noreply.github.com>
2022-05-18 15:48:46 +1000

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let is_running = false;
 let demo_button = document.getElementById("start_demo");
 let currentMovement = "1";
 
-console.log("v8");
+console.log("v9");
 
 const gainNode = new Tone.Gain(0).toDestination();
 const gainNode2 = new Tone.Gain(0).connect(gainNode);
@@ -160,6 +160,7 @@ demo_button.onclick = function (e) {
       Owl.start();
     }
     if (currentMovement === "5") {
+      gainNode.gain.rampTo(0, 0.1);
       Flute.start();
     }
     gainNode.gain.rampTo(1, 0.1);
