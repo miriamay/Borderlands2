0ed8f274dc310b9069b6da171b5986622643a15a
miriamay <105532752+miriamay@users.noreply.github.com>
2022-05-18 13:59:18 +1000

    Update script.js

--- script.js
+++ script.js
@@ -12,28 +12,15 @@ const gainNode = new Tone.Gain(0).toDestination();
 const gainNode2 = new Tone.Gain(0).connect(gainNode);
 const reverb = new Tone.Reverb(3).connect(gainNode);
 reverb.wet.value = 0.4;
-// const phaser = new Tone.Phaser({
-//   frequency: 15,
-//   octaves: 5,
-//   baseFrequency: 1000,
-// }).connect(reverb);
 const lowpass = new Tone.Filter({
   Q: 10,
   frequency: 18000,
   type: "lowpass",
 }).connect(reverb);
-const pitchShift = new Tone.PitchShift(0).connect(reverb);
-// const pluckedEnv = new Tone.AmplitudeEnvelope({
-//   attack: 0.05,
-//   decay: 0.1,
-//   sustain: 0.15,
-//   release: 0.1,
-//   decayCurve: "exponential",
-// }).connect(pitchShift);
 const Lyre = new Tone.Player({
   url: "https://miriamay.github.io/Borderlands/Audio/LyreNatural.mp3",
   onload: ready(),
-}).connect(pitchShift);
+}).connect(gainNode2);
 const Flute = new Tone.Player({
   url: "https://miriamay.github.io/Borderlands/Audio/Flute.mp3",
   loop: true,
@@ -59,7 +46,6 @@ const Witches = new Tone.Player(
 const Owl = new Tone.Player(
   "https://miriamay.github.io/Borderlands/Audio/OwlNatural.mp3"
 ).connect(gainNode2);
-Tone.Transport.start();
 
 function scaleValue(value, from, to) {
   let scale = (to[1] - to[0]) / (from[1] - from[0]);
@@ -75,39 +61,6 @@ let powerScale = d3
   .range([200, 8000])
   .clamp(true);
 
-const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
-
-let t1on = false;
-let accelActivate = 2;
-let accelDeactivate = 0.2;
-
-//trigger Frog
-function trigger2(accel) {
-  if (accel >= accelActivate) {
-    if (t1on) return;
-    t1on = true;
-    frogDict[Math.floor(Math.random() * 5)].start();
-  }
-  if (accel < accelDeactivate) {
-    t1on = false;
-  }
-}
-
-//trigger Flute Mimicry
-function trigger5(accel) {
-  if (accel >= accelActivate) {
-    if (t1on) return;
-    t1on = true;
-    gainNode2.gain.rampTo(0.8, 0.1);
-    setTimeout(function () {
-      gainNode2.gain.rampTo(0, 0.5);
-    }, 1000);
-  }
-  if (accel < accelDeactivate) {
-    t1on = false;
-  }
-}
-
 const frogDict = {
   1: Frog1,
   2: Frog2,
@@ -120,7 +73,7 @@ movement.onchange = function () {
   currentMovement = movement.value;
   if (currentMovement !== "1") {
     Lyre.stop();
-    pitchShift.pitch = 0;
+    //pitchShift.pitch = 0;
   } else {
     reverb.wet.value = 0.4;
     reverb.decay = 3;
@@ -138,43 +91,20 @@ movement.onchange = function () {
 };
 
 function handleOrientation(event) {
-  if (currentMovement === "1") {
-    if (event.beta < 10) pitchShift.pitch = 5;
-    if (10 <= event.beta && event.beta < 60) pitchShift.pitch = 0;
-    if (60 <= event.beta && event.beta < 100) pitchShift.pitch = -2;
-    if (event.beta >= 100) pitchShift.pitch = -9;
-  }
-  if (currentMovement === "3") {
-    lowpass.frequency.value = powerScale(Math.abs(event.beta));
-  }
-  if (currentMovement === "4") {
-    //lowpass.frequency.value = powerScale(event.beta);
+  if (currentMovement === "1" || currentMovement === "4") {
     gainNode2.gain.rampTo(
       scaleValue(Math.abs(event.beta), [0, 180], [0, 1]),
       0.1
     );
-    // phaser.frequency.value = scaleValue(event.beta, [-50, 150], [0, 15]);
-    // phaser.baseFrequency = scaleValue(
-    //   Math.abs(event.gamma),
-    //   [0, 90],
-    //   [100, 2000]
-    // );
+  }
+  if (currentMovement === "3") {
+    lowpass.frequency.value = powerScale(Math.abs(event.beta));
   }
   if (currentMovement === "5") {
     Flute.playbackRate = scaleValue(event.beta, [-50, 150], [0.25, 2.5]);
     Flute.loopStart = scaleValue(Math.abs(event.gamma), [0, 90], [0, 140]);
     Flute.loopEnd = Flute.loopStart + 1;
   }
-}
-
-let accel;
-function handleMotion(event) {
-  accel =
-    event.acceleration.x ** 2 +
-    event.acceleration.y ** 2 +
-    event.acceleration.z ** 2;
-  if (currentMovement === "2") trigger2(accel);
-  if (currentMovement === "5") trigger5(accel);
 }
 
 function ready() {
@@ -196,8 +126,10 @@ demo_button.onclick = function (e) {
   if (is_running) {
     window.removeEventListener("devicemotion", handleMotion);
     window.removeEventListener("deviceorientation", handleOrientation);
+    window.removeEventListener("shake", shakeEventDidOccur, false);
     demo_button.innerHTML = "START";
     document.getElementById("circle").style.background = "green";
+    myShakeEvent.stop();
     gainNode.gain.rampTo(0, 0.1);
     Lyre.stop();
     Flute.stop();
@@ -207,16 +139,18 @@ demo_button.onclick = function (e) {
   } else {
     window.addEventListener("devicemotion", handleMotion);
     window.addEventListener("deviceorientation", handleOrientation);
+    window.addEventListener("shake", shakeEventDidOccur, false);
     document.getElementById("start_demo").innerHTML = "STOP";
     document.getElementById("circle").style.background = "red";
+    myShakeEvent.start();
     if (currentMovement === "1") {
-      Lyre.sync().start(0);
+      Lyre.start();
     }
     if (currentMovement === "3") {
-      Witches.sync().start(0);
+      Witches.start();
     }
     if (currentMovement === "4") {
-      Owl.sync.start(0);
+      Owl.start();
     }
     if (currentMovement === "5") {
       Flute.start();
@@ -234,7 +168,27 @@ document.addEventListener("visibilitychange", function () {
     demo_button.innerHTML = "START";
     document.getElementById("circle").style.background = "green";
     gainNode.gain.rampTo(0, 0.1);
-    Tone.Transport.pause();
+    Lyre.stop();
+    Flute.stop();
+    Witches.stop();
+    Owl.stop();
     is_running = false;
   }
 });
+
+var myShakeEvent = new Shake({
+  threshold: 1, // optional shake strength threshold
+  timeout: 1000, // optional, determines the frequency of event generation
+});
+
+//function to call when shake occurs
+function shakeEventDidOccur() {
+  if (currentMovement === "2") frogDict[Math.floor(Math.random() * 5)].start();
+  if (currentMovement === "5") {
+    gainNode2.gain.rampTo(1, 0.1);
+    setTimeout(function () {
+      gainNode2.gain.rampTo(0, 0.5);
+    }, 1000);
+  }
+  //alert('shake!');
+}
